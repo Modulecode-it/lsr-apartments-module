9ab3ee42c61dbe306d464bd04336dadb77e5b351
Titov Alexey <titov.alexey@uchitel-izd.ru>
2024-10-22 09:20:47 +0300

    Подключен композер и автозагрузка классов

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+global $APPLICATION;
 $APPLICATION->SetTitle("Модуль \"квартиры\"");
 ?>
 Модуль "квартиры".
+<?php \Lsr\Test::test(); ?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
