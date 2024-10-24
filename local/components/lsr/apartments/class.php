a22c98c1b0cda02ffcea3a77ae52918c5fb4a588
Titov Alexey <titov.alexey@uchitel-izd.ru>
2024-10-24 11:44:49 +0300

    Публичная часть. Вывод квартир без изображений.

--- local/components/lsr/apartments/class.php
+++ local/components/lsr/apartments/class.php
@@ -1,4 +1,8 @@
 <?php
+
+use Bitrix\Main\UI\PageNavigation;
+use Lsr\Model\ApartmentTable;
+
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 /**
@@ -6,8 +10,35 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
  */
 class CLsrApartmentsComponent extends CBitrixComponent
 {
+	const FILTER = [ApartmentTable::ACTIVE => 'Y', ApartmentTable::STATUS => ApartmentTable::STATUS_SALE];
+
 	public function executeComponent()
 	{
+		// Создаем объект для постраничной навигации
+		$nav = new PageNavigation("nav");
+		$nav->allowAllRecords(true) // Разрешить показывать все записи на одной странице (опционально)
+			->setPageSize(10)
+			->initFromUri(); // Инициализируем из URI (для правильной работы с ?page=2 и т.д.)
+		$nav->setRecordCount($this->getCountByFilter());
+
+		$this->arResult['COLLECTION'] = $this->getCollection($nav);
+		$this->arResult['NAV'] = $nav;
 		$this->IncludeComponentTemplate();
+	}
+
+	private function getCollection(PageNavigation $nav): \Bitrix\Main\ORM\Objectify\Collection
+	{
+		return ApartmentTable::getList([
+			'select' => ['*', ApartmentTable::HOUSE/*, ApartmentTable::IMAGES*/],
+			'filter' => self::FILTER,
+			'order' => [ApartmentTable::HOUSE_ID => 'DESC', ApartmentTable::NUMBER => 'ASC'],
+			'offset' => $nav->getOffset(),              // Смещение для текущей страницы
+			'limit' => $nav->getLimit(),
+		])->fetchCollection();
+	}
+
+	private function getCountByFilter(): int
+	{
+		return ApartmentTable::getList(['filter' => self::FILTER,])->getSelectedRowsCount();
 	}
 }
--- local/components/lsr/apartments/templates/.default/template.php
+++ local/components/lsr/apartments/templates/.default/template.php
@@ -1,7 +1,56 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
+/**
+ * @var array $arResult
+ */
+
+/**
+ * @var \Bitrix\Main\ORM\Objectify\Collection $collection
+ */
+$collection = $arResult['COLLECTION'];
+
 ?>
 
 <h1>Квартиры в продаже</h1>
+
+<?php if($collection->count()): ?>
+	<?php /** @var Bitrix\Main\ORM\Entity $item  */ ?>
+	<?php foreach ($collection as $item): ?>
+		<div>
+			<h4>Квартира № <?= $item->getNumber() ?></h4>
+			<div>Стоимость: <?= $item->getPrice() ?></div>
+			<? if($item->getSalePrice()): ?>
+				<div>Стоимость со скидкой: <?= $item->getSalePrice() ?></div>
+			<? endif; ?>
+			<div>Адрес: <?= $item->getHouse()->getAddress() ?></div>
+
+			<div>
+<!--				--><?php //foreach ($item->getImages() as $image): ?>
+<!--					--><?php //\CFile::ShowFile($image->getFileId()) ?>
+<!--				--><?php //endforeach; ?>
+			</div>
+		</div>
+	<?php endforeach; ?>
+
+
+	<?php
+	global $APPLICATION;
+	$APPLICATION->IncludeComponent(
+		'bitrix:main.pagenavigation',
+		'',
+		array(
+			'NAV_OBJECT' => $arResult['NAV'],
+			'SEF_MODE' => 'N',
+		),
+		$component
+	);
+
+
+	?>
+<?php else:  ?>
+	<div class="alert alert-warning" role="alert">
+		Все квартиры раскупили. Но мы уже строим новые!
+	</div>
+<?php endif; ?>
 
 
