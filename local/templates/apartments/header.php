09df955106c769f2083bda0e68a635dcc0fc190f
Titov Alexey <titov.alexey@uchitel-izd.ru>
2024-10-24 09:35:11 +0300

    Публичная часть. Подключил бутстрап

--- local/templates/apartments/header.php
+++ local/templates/apartments/header.php
@@ -3,12 +3,15 @@ IncludeTemplateLangFile(__FILE__);
 global $APPLICATION;
 global $USER;
 ?>
-<html>
-<head>
-<?$APPLICATION->ShowHead();?>
-<title><?$APPLICATION->ShowTitle()?></title>
-</head>
+	<!doctype html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?$APPLICATION->ShowHead();?>
+		<title><?$APPLICATION->ShowTitle()?></title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	</head>
+	<body>
+	<?$APPLICATION->ShowPanel()?>
 
-<body>
-
-<?$APPLICATION->ShowPanel()?>
