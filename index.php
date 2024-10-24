ed41774ea0da5fc63f4289d8615375de6bfb9e1d
Titov Alexey <titov.alexey@uchitel-izd.ru>
2024-10-24 09:24:49 +0300

    Публичная часть. Заготовка компонента.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 global $APPLICATION;
 $APPLICATION->SetTitle("Модуль \"квартиры\"");
 ?>
-Модуль "квартиры".
+
+<?php $APPLICATION->IncludeComponent("lsr:apartments", ""); ?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/components/lsr/apartments/.description.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => "Квартиры в продаже",
+	"DESCRIPTION" => "Отображает список квартир в продаже с возможностью фильтрации по ним"
+);
