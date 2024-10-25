a23c36caff1ae43f161bd5a70423187d06270161
Titov Alexey <titov.alexey@uchitel-izd.ru>
2024-10-25 17:42:15 +0300

    Упаковка в модуль. Удален композер и автозагрузка.

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -6,7 +6,5 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 	die();
 }
 
-//require_once($_SERVER["DOCUMENT_ROOT"] . "/local/vendor/autoload.php");
-
 CModule::IncludeModule("modulecode.lsrapartments");
 (new Bootstrap())->bootstrap();
