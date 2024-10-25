3530823bd096f2af2c5d5cc9048fec37499c85ce
Titov Alexey <titov.alexey@uchitel-izd.ru>
2024-10-25 18:36:51 +0300

    Упаковка в модуль. Перенес подключение бутстрапа в модуль

--- local/modules/modulecode.lsrapartments/admin/apartments_edit.php
+++ local/modules/modulecode.lsrapartments/admin/apartments_edit.php
@@ -1,13 +1,16 @@
 <?php
+
 require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_before.php");
 
-$classToEdit = \Modulecode\Lsrapartments\Model\ApartmentTable::class;
-$imagesClass = \Modulecode\Lsrapartments\Model\ApartmentImageTable::class;
-$backurl = '/bitrix/admin/lsr_apartments_list.php';
-$tabName = 'Квартира';
-$tabTitle = 'Параметры:';
-
-require_once("common_edit.php");
-
+if(CModule::IncludeModule('modulecode.lsrapartments')) {
+	$classToEdit = \Modulecode\Lsrapartments\Model\ApartmentTable::class;
+	$imagesClass = \Modulecode\Lsrapartments\Model\ApartmentImageTable::class;
+	$backurl = '/bitrix/admin/lsr_apartments_list.php';
+	$tabName = 'Квартира';
+	$tabTitle = 'Параметры:';
+	require_once("common_edit.php");
+} else {
+	ShowError("Модуль ЛСР.Квартиры не подключен");
+}
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_admin.php");
--- local/modules/modulecode.lsrapartments/admin/houses_edit.php
+++ local/modules/modulecode.lsrapartments/admin/houses_edit.php
@@ -1,14 +1,15 @@
-<?
+<?php
 require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_before.php");
 
-$classToEdit = \Modulecode\Lsrapartments\Model\HouseTable::class;
-$imagesClass = \Modulecode\Lsrapartments\Model\HouseImageTable::class;
-$backurl = '/bitrix/admin/lsr_houses_list.php';
-$tabName = 'Дом';
-$tabTitle = 'Параметры:';
-
-require_once("common_edit.php");
-
+if(CModule::IncludeModule('modulecode.lsrapartments')) {
+	$classToEdit = \Modulecode\Lsrapartments\Model\HouseTable::class;
+	$imagesClass = \Modulecode\Lsrapartments\Model\HouseImageTable::class;
+	$backurl = '/bitrix/admin/lsr_houses_list.php';
+	$tabName = 'Дом';
+	$tabTitle = 'Параметры:';
+	require_once("common_edit.php");
+} else {
+	ShowError("Модуль ЛСР.Квартиры не подключен");
+}
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_admin.php");
-?>
