394baaabff77c3a2762dc76aa44e9142466c5f07
Awitaminosis <shapochkin.evgeniy@uchitel-izd.ru>
2024-10-28 10:14:20 +0300

    lang для вызова списка

--- local/modules/modulecode.lsrapartments/admin/houses_list.php
+++ local/modules/modulecode.lsrapartments/admin/houses_list.php
@@ -12,11 +12,11 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admi
 if(CModule::IncludeModule('modulecode.lsrapartments')) {
 	$classToList = \Modulecode\Lsrapartments\Model\HouseTable::class;
 	$tableId = 'lsr_houses';
-	$titleForList = 'Список домов';
+	$titleForList = GetMessage("TITLE_FOR_LIST");
 	$editPhpUrl = 'lsr_houses_edit.php';
 	require_once("common_list.php");
 } else {
-	ShowError("Модуль ЛСР.Квартиры не подключен");
+	ShowError(GetMessage("INCLUDE_ERROR"));
 }
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_admin.php");
