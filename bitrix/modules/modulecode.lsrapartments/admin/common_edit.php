1345d68ab17f3b77e3eee47dfe31e60251cf9e36
Awitaminosis <shapochkin.evgeniy@uchitel-izd.ru>
2024-10-29 11:52:44 +0300

    улучшены заголовки на странице редактирования

--- bitrix/modules/modulecode.lsrapartments/admin/common_edit.php
+++ bitrix/modules/modulecode.lsrapartments/admin/common_edit.php
@@ -24,7 +24,6 @@ $context = $instance->getContext();
 $request = $context->getRequest();
 $server = $context->getServer();
 $lang = $context->getLanguage();
-$pageTitle = GetMessage("TITLE_REDACT") . $tabName;
 
 $id = (int)$request->get('ID');
 
@@ -203,7 +202,7 @@ if ($server->getRequestMethod() == "POST"
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_admin_after.php");
 
-$APPLICATION->SetTitle(($id > 0) ? $pageTitle . ' № ' . $id : $pageTitle);
+$APPLICATION->SetTitle(($id > 0) ? $tabName . ' № ' . $id . ': ' . GetMessage("TITLE_REDACT") : $tabName . ': ' . GetMessage("TITLE_ADD"));
 
 $aTabs = array(
 	array(
